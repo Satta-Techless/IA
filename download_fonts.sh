3459977bb4d06996b597c7dc711afe519fc9423a
Satta X <satta4959@gmail.com>
2026-07-31 17:24:53 +0300

    Add files via upload

---
+++ download_fonts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Downloading Inter fonts for cross-platform poster generation..."
+wget -q -O Inter-Bold.ttf https://github.com/rsms/inter/raw/master/fonts/ttf/Inter-Bold.ttf
+wget -q -O Inter-Regular.ttf https://github.com/rsms/inter/raw/master/fonts/ttf/Inter-Regular.ttf
+echo "Fonts downloaded."
